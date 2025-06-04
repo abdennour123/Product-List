d8214db0c9eae13752a673f60fb5e5d679710fab
Abdenour <abdnourhajy333@gmail.com>
2025-06-04 13:21:10 +0800

    adding items to shopping cart and control the quantity

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// get elements from the DOM
+const emptyCart = document.querySelector('.empty-cart');
+const shoppingCart = document.querySelector('.shopping-cart');
+const cartTitle = document.querySelector('.cart-title');
+let quantity = 0;
+let numOfProducts = 0;
+
 fetch('./assets/data.json')
     .then(response => response.json())
     .then(data =>{
@@ -20,7 +27,7 @@ fetch('./assets/data.json')
             addBtn.classList.add('addCardBtn');
             addBtn.textContent = "Add To Card";
             cardBody.appendChild(addBtn);
-
+            
             const cardicon = document.createElement('i'); // add to card button icon
             cardicon.classList.add("fa-solid", "fa-cart-plus")
             addBtn.appendChild(cardicon);
@@ -37,12 +44,72 @@ fetch('./assets/data.json')
             price.textContent = `$${item.price.toFixed(2)}`; 
             cardBody.appendChild(price);
 
-            // Append body to card
-            card.appendChild(cardBody);
+            card.appendChild(cardBody); // Append body to card          
+            container.appendChild(card); // Append card to container
 
-            // Append card to container
-            container.appendChild(card);
+            // add quantity and numOfProducts when the button is clicked
+
+            const quantityBtn = document.createElement("div");
+            quantityBtn.classList.add('quantityBtn');
+            const plusbtn = document.createElement("button"); // plus button
+            plusbtn.classList.add('plusBtn');
+            plusbtn.textContent = "+";
+            const minusbtn = document.createElement("button"); // minus button
+            minusbtn.classList.add('minusBtn');
+            minusbtn.textContent = "-";
+            const quantityDisplay = document.createElement("span"); // quantity display
+            quantityDisplay.classList.add('quantityDisplay');
+
+            quantityBtn.appendChild(minusbtn);
+            quantityBtn.appendChild(quantityDisplay);
+            quantityBtn.appendChild(plusbtn);
+            
+            // Add event listener to the button
+            addBtn.addEventListener("click", ()=>{
+                emptyCart.classList.add("active");
+                quantity = 1;
+                numOfProducts += 1;
+                cardBody.replaceChild(quantityBtn, addBtn);
+                quantityDisplay.textContent = quantity;
+                const Items = document.createElement("div"); // item in cart
+                Items.classList.add('item');
+                Items.innerHTML = `
+                    <img src="${item.image.thumbnail}" alt="${item.name}">
+                    <div class="item-details">
+                        <h4>${item.name}</h4>
+                        <p>Quantity: ${quantity}</p>
+                        <p>Price: $${(item.price * quantity)}</p>
+                    </div>
+                `
+                shoppingCart.appendChild(Items);
+            })
+            plusbtn.addEventListener("click", () => {
+                quantity++;
+                quantityDisplay.textContent = quantity;
+                if (quantity > 20) {
+                    quantity = 20; // limit to 20
+                    quantityDisplay.textContent = quantity;
+                }
+            })
+            minusbtn.addEventListener("click", () => {
+                quantity--;
+                quantityDisplay.textContent = quantity;
+                if(quantity < 1){
+                    cardBody.replaceChild(addBtn, quantityBtn);
+                    emptyCart.classList.remove("active");
+                    // Remove the item from the shopping cart
+                    const cartItems = shoppingCart.querySelectorAll('.item');
+                    cartItems.forEach(cartItem => {
+                    const itemTitle = cartItem.querySelector('h3').textContent;
+                    if (itemTitle === item.name) {
+                        cartItem.remove();
+                        }
+                    }); 
+                }
+            })
     });
 }).catch(error => {
     console.error('Error loading JSON data:', error);
 });
+
+
